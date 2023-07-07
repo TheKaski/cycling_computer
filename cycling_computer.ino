7bae91fa7a9caf5a96c10301626e1de9da85fe8c
Aaro Halme <aaro.halme2000@gmail.com>
2023-07-07 19:51:28 +0300

    Better formatting

--- cycling_computer.ino
+++ cycling_computer.ino
@@ -1,9 +1,9 @@
 
-const int buttonPin = A2;  // the number of the pushbutton pin
+const int buttonPin = A2;  // speedSensorPin
 const int ledPin = 13;    // the number of the LED pin
 
-boolean clock_is_on = false; // Variable for  determining the clock state
-boolean has_gone_low = false; // Variable for detectinng the start of the clock
+boolean clockEnabled = false; // Variable for  determining the clock state
+boolean hasGoneLow = false; // Variable for detectinng the start of the clock
 
 unsigned long int T1, T2, timeDiff; //Time values
 int buttonState = 0;  // variable for reading the pushbutton status
@@ -15,33 +15,41 @@ void setup() {
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 }
+
 void loop() {
   timeDiff = speedSensorPulse();
+  printToSerial(timeDiff);
+}
+
+void printToSerial(unsigned int long value) {
+  if ( value > 5 ) {
+    Serial.println(value);
+  }
 }
 
 unsigned long int speedSensorPulse() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
   // Start the clock when HIGH
-  if (buttonState == HIGH and has_gone_low == true) {
+  if (buttonState == HIGH and hasGoneLow == true) {
     //If clock is started take T2 and stop the clock
-    if(clock_is_on == true){
+    if(clockEnabled == true){
       T2 = millis();
-      clock_is_on = false;
-      if(T2-T1 > 5){
-        Serial.println(T2-T1); 
-      }  
-      has_gone_low = false;
+      clockEnabled = false;
+      hasGoneLow = false;
     }
     T1 = millis();
-    clock_is_on = true;
+    clockEnabled = true;
     // turn LED on:
     digitalWrite(ledPin, HIGH);
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
-    has_gone_low = true;
+    hasGoneLow = true;
   }
+
+  return T2-T1;
+
 }
 
 void calculate_speed_from_time()
