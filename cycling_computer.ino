1a95582a5254d106068b8da0c7c7f53b6584e18d
Aaro Halme <aaro.halme2000@gmail.com>
2023-07-25 16:03:57 +0300

    Cleaned non-interrupt version parts out

--- cycling_computer.ino
+++ cycling_computer.ino
@@ -1,15 +1,11 @@
 
 // Setting pins
-const int speedSensor = A2;  // speedSensorPin
+const int speedSensor = 2;  // speedSensorPin
 const int ledPin = 13;  // the number of the LED pin
 
 // Setting clock variables
 boolean clockEnabled = false;  // Variable for determining the clock state
-boolean hasGoneLow = false;  // Variable for detecting the start of the clock
 unsigned long int T1, T2, timeDiff;  // Time values
-
-// Setting other variables
-int speedSensorState = 0;  // variable for reading the speedSensor status
 
 // User settings:
 float wheelSize = 27.5; // Wheel size in inches
@@ -36,34 +32,11 @@ void loop() {
   printToSerial(timeDiff);   
   
 }
+
 void printToSerial(unsigned int long value) {
   if ( value > 100 ) { //100 ms 27.5 inch wheel max speed is 78..km/h 
     Serial.println(value);
   }
-}
-unsigned long int speedSensorPulse() {
-  unsigned long int diff;
-  // read the state of the pushbutton value:
-  speedSensorState = digitalRead(speedSensor);
-  // Start the clock when HIGH
-  if (speedSensorState == HIGH and hasGoneLow == true) {
-    //If clock is started take T2 and stop the clock
-    if(clockEnabled == true){
-      T2 = millis();
-      clockEnabled = false;
-      hasGoneLow = false;
-      diff = T2-T1;
-    }
-    T1 = millis();
-    clockEnabled = true;
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-    hasGoneLow = true;
-  }
-  return diff;
 }
 
 //This is a interrupt function which will take Time T1 or T2 based on clock status
